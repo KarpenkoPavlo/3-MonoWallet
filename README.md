3d5201e353b678bf3a45771d80d28e916e597042
Pavlo Karpenko <karpenkopasha19@outlook.com>
2023-10-22 16:32:59 +0300

    added makrdown file/hover/not(hover)/sticky header

---
+++ README.md
@@ -0,0 +1,12 @@
+<h1>Mono Wallet</h1>
+
+![Foto](/img/header/MonoWalletMD.png)
+
+---
+
+<h6>Layout of a one-page site using HTML,CSS,JavaScript.</h6>
+
+<h6>A one-page bank website where the user can conduct transactions in his online wallet.</h6>
+
+---
+<h6>Finished</h6>
--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-ss
+/*header add background*/
+let headerBackground = document.querySelector(".header");
+
+window.onscroll = function() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        headerBackground.style.backgroundColor = "rgb(5%, 5%, 5%, 0.7)";
+    } else {
+        headerBackground.style.backgroundColor = "rgb(1%, 1%, 1%, 0.0)";
+    }
+  };
